d012622358d33935028fe338064938620fab243b
truongphi <truongphi000@gmail.com>
2019-07-02 16:44:49 +0700

    ssssss

---
+++ resources/views/auth/activeUser.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Activation Email Todo List</title>
+</head>
+<body>
+<h4>Welcome you to be a user of todo list. </h4>
+<p>
+    </br>
+    <a href="{{ $link }}">Click here to validate your account.</a>
+</p>
+</body>
+</html>
